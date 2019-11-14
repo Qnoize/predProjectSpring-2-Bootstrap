2068b6e1d10e986f9e573b16e062a7a3d385f8f0
alekseydanilov <qnoize@gmail.com>
2019-11-14 22:34:36 +0300

    last

--- src/main/java/ru/jmentor/model/User.java
+++ src/main/java/ru/jmentor/model/User.java
@@ -3,7 +3,6 @@ package ru.jmentor.model;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import java.util.*;
 import java.util.stream.Collectors;
